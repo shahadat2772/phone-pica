7c78208566d89306821675d02bed657bb14c82f1
shahadat2772 <shahadat2772@gmailcom>
2022-02-28 22:16:37 +0600

    cards added

--- js/app.js
+++ js/app.js
@@ -3,4 +3,56 @@ fetch("https://openapi.programming-hero.com/api/phones?search=iphone")
   .then((res) => res.json())
   .then((data) => console.log(data));
 
-// GETTING
+//   GETTING INPUT FIELD VALUE
+const inputField = document.getElementById("inputField");
+
+// GETTING PHONES CONTAINER
+const phonesContainer = document.getElementById("phonesContainer");
+
+// LOADING DATA AND BY SEARCH
+const getPhones = () => {
+  const url = `https://openapi.programming-hero.com/api/phones?search=${inputField.value}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => showPhonesOnSite(data.data));
+  // console.log(inputField.value);
+};
+
+{
+  /* <div class="col">
+  <div class="card h-100">
+    <img src="..." class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">
+        This is a longer card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </p>
+    </div>
+  </div>
+</div>; */
+}
+
+// SHOWIGN PHONES ON SITE
+const showPhonesOnSite = (phones) => {
+  phones.forEach((phone) => {
+    // console.log(phone);
+    const phoneDiv = document.createElement("div");
+    phoneDiv.classList.add("col");
+    phoneDiv.innerHTML = `
+    <div class="card h-100">
+    <div class=" d-flex justify-content-center">
+    <img src="${phone.image}" class="card-img-top p-2 w-75" alt="..." />
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">${phone.phone_name}</h5>
+      <p class="card-text">
+        This is a longer card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </p>
+    </div>
+  </div>
+    `;
+    phonesContainer.appendChild(phoneDiv);
+  });
+};
