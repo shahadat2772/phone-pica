8e4877ecd067d5d0ece2661aa11c27bc3ad12ba1
shahadat2772 <shahadat2772@gmailcom>
2022-03-01 00:16:01 +0600

    phone cards completed and error handeler added

--- js/app.js
+++ js/app.js
@@ -9,78 +9,56 @@ const inputField = document.getElementById("inputField");
 // GETTING PHONES CONTAINER
 const phonesContainer = document.getElementById("phonesContainer");
 
+// ERROR MASSASE DIV
+const error = document.getElementById("error");
+
 // LOADING DATA AND BY SEARCH
 const getPhones = () => {
-  const url = `https://openapi.programming-hero.com/api/phones?search=${inputField.value}`;
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => showPhonesOnSite(data.data));
-  // console.log(inputField.value);
+  if (inputField.value === "") {
+    error.innerText = "Please enter a name!";
+  } else {
+    const url = `https://openapi.programming-hero.com/api/phones?search=${inputField.value}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => showPhonesOnSite(data));
+  }
+  //CLEARING INPUT FIELD
+  inputField.value = "";
 };
 
-{
-  /* <div class="col">
-  <div class="card h-100">
-    <img src="..." class="card-img-top" alt="..." />
-    <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">
-        This is a longer card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </p>
-    </div>
-  </div>
-</div>; */
-}
-
-// // SHOWIGN PHONES ON SITE
-// const showPhonesOnSite = (phones) => {
-//   phones.forEach((phone) => {
-//     // console.log(phone);
-//     const phoneDiv = document.createElement("div");
-//     phoneDiv.classList.add("col");
-//     phoneDiv.classList.add("phoneCards");
-//     phoneDiv.innerHTML = `
-//     <div class="card h-100 phoneCards">
-//     <div class=" d-flex justify-content-center">
-//     <img src="${phone.image}" class="card-img-top p-2 w-75" alt="..." />
-//     </div>
-//     <div class="card-body">
-//       <h5 class="card-title">${phone.phone_name}</h5>
-//       <p class="card-text">
-//         This is a longer card with supporting text below as a natural lead-in to
-//         additional content. This content is a little bit longer.
-//       </p>
-//     </div>
-//   </div>
-//     `;
-//     phonesContainer.appendChild(phoneDiv);
-//   });
-// };
-
 // SHOWIGN PHONES ON SITE
-const showPhonesOnSite = (phones) => {
-  for (let i = 0; i < 20; i++) {
-    const phoneDiv = document.createElement("div");
-    phoneDiv.classList.add("col");
-    phoneDiv.classList.add("phoneCards");
-    phoneDiv.innerHTML = `
+const showPhonesOnSite = (data) => {
+  // CLEARING PHONE CONTAINER
+  phonesContainer.textContent = "";
+  const phones = data.data;
+  //   console.log(phones);
+  if (data.status === false) {
+    error.innerText = "No match found!";
+    // CLEARING PHONE CONTAINER
+    phonesContainer.textContent = "";
+  } else {
+    const first20Phones = phones.slice(0, 20);
+    first20Phones.forEach((phone) => {
+      // console.log(phone);
+      const phoneDiv = document.createElement("div");
+      phoneDiv.classList.add("col");
+      phoneDiv.classList.add("phoneCards");
+      phoneDiv.innerHTML = `
       <div class="card h-100 phoneCards">
       <div class=" d-flex justify-content-center">
-      <img src="${phones[i].image}" class="card-img-top p-2 w-75" alt="..." />
+      <img src="${phone.image}" class="card-img-top p-2 w-75" alt="..." />
       </div>
       <div class="card-body">
-        <h5 class="card-title">${phones[i].phone_name}</h5>
+        <h5 class="card-title">${phone.phone_name}</h5>
         <p class="card-text">
-          This is a longer card with supporting text below as a natural lead-in to
-          additional content. This content is a little bit longer.
+        Brand: ${phone.brand} <br>
+        <button class="moreBtn border-0 px-2 mt-2">More</button>
         </p>
       </div>
     </div>
       `;
-    phonesContainer.appendChild(phoneDiv);
+      phonesContainer.appendChild(phoneDiv);
+    });
+    error.innerText = "";
   }
-  //   phones.forEach((phone) => {
-  //     // console.log(phone);
-  //   });
 };
